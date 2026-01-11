5031b385d1a9ebf55454085209030d41cfdbc3e1
farmage <farmage@protonmail.com>
2026-01-11 13:38:54 +0400

    Wallet: fix empty checkpoints max height

--- src/checkpoints/checkpoints.cpp
+++ src/checkpoints/checkpoints.cpp
@@ -137,6 +137,8 @@ namespace cryptonote
   //---------------------------------------------------------------------------
   uint64_t checkpoints::get_max_height() const
   {
+    if (m_points.empty())
+      return 0;
     std::map< uint64_t, crypto::hash >::const_iterator highest = 
         std::max_element( m_points.begin(), m_points.end(),
                          ( boost::bind(&std::map< uint64_t, crypto::hash >::value_type::first, _1) < 
